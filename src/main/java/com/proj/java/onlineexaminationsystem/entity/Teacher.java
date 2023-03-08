084eafb368625279c69a4d006ba8620c08b02e6c
dhrumilpatel30 <dhrumilp063@gmail.com>
2023-03-08 20:31:17 +0530

    enitites change

--- src/main/java/com/proj/java/onlineexaminationsystem/entity/Teacher.java
+++ src/main/java/com/proj/java/onlineexaminationsystem/entity/Teacher.java
@@ -22,4 +22,66 @@ public class Teacher {
     private String email;
     @Column
     private Date date_of_birth;
+
+    public Teacher() {
+    }
+
+    public Teacher(int teacher_id, String subject, String name, String email, Date date_of_birth) {
+        this.teacher_id = teacher_id;
+        this.subject = subject;
+        this.name = name;
+        this.email = email;
+        this.date_of_birth = date_of_birth;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "teacher_id=" + teacher_id +
+                ", subject='" + subject + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", date_of_birth=" + date_of_birth +
+                '}';
+    }
+
+    public int getTeacher_id() {
+        return teacher_id;
+    }
+
+    public void setTeacher_id(int teacher_id) {
+        this.teacher_id = teacher_id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
 }
